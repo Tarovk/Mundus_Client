6b390d0b6c5ea634f669fd4ad073cd39c4425817
makchich <m.akchich@student.tudelft.nl>
2017-01-24 00:36:56 +0100

    Add documentation to AppDelegate.swift

--- Mundus_ios/AppDelegate.swift
+++ Mundus_ios/AppDelegate.swift
@@ -9,6 +9,9 @@
 import UIKit
 import Aldo
 
+/**
+    Singleton containing methods that are called in the lifetime of the app.
+ */
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -22,11 +25,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+    /// Forces the device to set the orientation to portrait.
     func forcePortraitOrientation() {
         let orientation = UIInterfaceOrientation.portrait.rawValue
         UIDevice.current.setValue(orientation, forKey: "orientation")
     }
 
+    /// Forces the device to set the orientation to landscape.
     func forceLandscapeOrientation() {
         let orientation = UIInterfaceOrientation.landscapeLeft.rawValue
         UIDevice.current.setValue(orientation, forKey: "orientation")
@@ -36,7 +41,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window: UIWindow?) -> UIInterfaceOrientationMask {
         return isShowingSources ? UIInterfaceOrientationMask.landscapeLeft : UIInterfaceOrientationMask.portrait
     }
-    
+
+    /// Clears all information about a stored session.
     class func clearSessionData() {
         Aldo.getStorage().removeObject(forKey: Aldo.Keys.SESSION.rawValue)
     }
